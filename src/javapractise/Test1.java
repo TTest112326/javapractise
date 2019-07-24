2c28889aaa5eeb71354f7d430953a8bf2c0e44f9
TTest112326 <test.t112326@gmail.com>
2019-07-24 08:48:21 +0530

    first commit through eclipse

---
+++ src/javapractise/Test1.java
@@ -0,0 +1,15 @@
+package javapractise;
+
+public class Test1 {
+
+	public static void main(String[] args) 
+	{
+		int a=0;
+		if(a==0)
+			System.out.println("flag is true");
+		else
+			System.out.println("flag is false");
+
+	}
+
+}
